60eab2bb8ab8fdc40287c3cd92ea52e6c7f7e325
Karim Bassem <kimobisso@gmail.com>
2025-02-22 18:30:35 +0200

    added the forget password and improved it's UI

--- polls_app/urls.py
+++ polls_app/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path('', views.index, name="index"),
     path("register/", auth.register, name="register"),
     path("login/", auth.user_login, name="login"),
-    path('password_reset/', auth.CustomPasswordResetView.as_view() , name='password_reset'),
-    path('password_reset/done/',auth.CustomPasswordResetDoneView.as_view() , name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',auth.CustomPasswordResetConfirmView.as_view() , name='password_reset_confirm'),
-    path('reset/done/', auth.CustomPasswordResetCompleteView.as_view() , name='password_reset_complete'),
+    path('password_reset/', auth.CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth.CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth.CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('activate/<uidb64>/<token>/', auth.activate, name='activate'),
 ]
