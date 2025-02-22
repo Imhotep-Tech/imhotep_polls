1262ead265ea942d8827fc9a77b3fc24edfb797b
Karim Bassem <kimobisso@gmail.com>
2025-02-22 21:12:09 +0200

    added the admin user

--- polls_app/admin.py
+++ polls_app/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Poll, Choice
+from .models import Poll, Choice, User
+
+# Unregister the models if they are already registered
+for model in [Poll, Choice, User]:
+    if model in admin.site._registry:
+        admin.site.unregister(model)
 
 # Register your models here.
 admin.site.register(Poll)
 admin.site.register(Choice)
+admin.site.register(User)
